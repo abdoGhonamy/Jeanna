c0c167e6f7bfb6a7a0c63ecbfe1d7f7a0d62638a
Abdelbadea <Abdo250102002@gmail.com>
2025-11-06 15:34:35 +0200

    Update

--- script.js
+++ script.js
@@ -1,16 +1,14 @@
-let countEl=document.getElementById("count-el")
-let saveEl=document.getElementById("savedstuff")
-// console.log(saveEl)
-let count = 0 ;
-function increment(){
-    count+=1 ; 
-    countEl.textContent = count;
+let countEl = document.getElementById("count-el");
+let saveEl = document.getElementById("savedstuff");
+let count = 0;
+
+function increment() {
+  count += 1;
+  countEl.textContent = count;
 }
 
-
-
-function save(){
-    saveEl.textContent += count + " - ";
-    countEl.innerText = 0;
-    count = 0 ;
+function save() {
+  saveEl.textContent += count + " - ";
+  countEl.textContent = 0;
+  count = 0;
 }
